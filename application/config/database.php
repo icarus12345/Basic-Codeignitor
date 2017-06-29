37311afee8bc25c7ed9bd4d26f8db9a8b75fb129
Truong Khuong <khuongxuantruong@gmail.com>
2017-06-29 16:25:31 +0700

    angular

--- application/config/database.php
+++ application/config/database.php
@@ -94,3 +94,24 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+$db['oauth'] = array(
+	'dsn'	=> 'mysql:dbname=oauth;host=127.0.0.1',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'oauth',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => TRUE,
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
